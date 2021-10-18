01aa4ec550c28385f44b457622153f1429e32463
JUAN FELIPE CASTILLO GOMEZ <juanfelipe.castillogomez@gmail.com>
2021-10-17 23:43:41 -0500

    Modificmodifications

--- test/model/Mini_MarketTest.java
+++ test/model/Mini_MarketTest.java
@@ -19,7 +19,7 @@ class Mini_MarketTest {
 		mini_market=new Mini_Market();
 	}
 	
-	
+	//(1) Puede ingresar al minimercado por cumplir todas las condiciones
 	@Test
 	public void testRegister1() {
 		setupScenary1();
@@ -50,11 +50,11 @@ class Mini_MarketTest {
 		}
 
 		ArrayList<User> userRegistered = mini_market.getUserRegistered();
-		assertEquals(1, userRegistered.size());
+		assertEquals(1, userRegistered.size());// El "1" significa que se registra en el arreglo
 	}
 	
 	
-	
+	//(2) El usuario que sí pudo ingresar, queda registrado en el arreglo total y en el arreglo de personas ingresadas
 	@Test
 	public void testRegister2() {
 		setupScenary1();
@@ -86,12 +86,12 @@ class Mini_MarketTest {
 		}
 
 		ArrayList<User> userRegistered = mini_market.getUserRegistered();
-		assertEquals(1, userRegistered.size());
-		assertEquals(1, mini_market.getAttemps());
+		assertEquals(1, userRegistered.size());// El "1" significa que se registra en el contador
+		assertEquals(1, mini_market.getAttemps());// El "1" significa que se registra en el arreglo
 	}
 	
 	
-	
+	//(3) NO puede ingresar al minimercado por ser menor de edad
 	@Test
 	public void testRegister3() {
 		setupScenary1();
@@ -109,11 +109,11 @@ class Mini_MarketTest {
 		}
 
 		ArrayList<User> userRegistered = mini_market.getUserRegistered();
-		assertEquals(0, userRegistered.size());
+		assertEquals(0, userRegistered.size());// El "0" significa que no se pudo registrar al arreglo
 	}
 	
 	
-	
+	//(4) NO se puede registrar porque no le corresponde el día de acuerdo con el número de su cédula y el día del mes
 	@Test
 	public void testRegister4() {
 		setupScenary1();
@@ -144,7 +144,7 @@ class Mini_MarketTest {
 		}
 
 		ArrayList<User> userRegistered = mini_market.getUserRegistered();
-		assertEquals(0, userRegistered.size());
+		assertEquals(0, userRegistered.size());// El "0" significa que no se pudo registrar al arreglo
 	}
 	
 	
